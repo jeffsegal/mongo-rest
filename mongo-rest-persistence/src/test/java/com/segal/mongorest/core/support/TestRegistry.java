8fbb96b94fed582c553cbb05df3a044a818567cb
Jeff Segal <jeffrey.segal@gmail.com>
2014-04-25 11:24:05 -0400

    Support for multiple DocumentProviders per DocumentType

--- mongo-rest-persistence/src/test/java/com/segal/mongorest/core/support/TestRegistry.java
+++ mongo-rest-persistence/src/test/java/com/segal/mongorest/core/support/TestRegistry.java
@@ -1,10 +1,11 @@
 package com.segal.mongorest.core.support;
 
-import com.segal.mongorest.core.annotation.DocumentType;
 import com.segal.mongorest.core.pojo.BaseDocument;
 import com.segal.mongorest.core.util.ClasspathAndBeanScanner;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,7 +20,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class TestRegistry extends ClasspathAndBeanScanner {
 
-	Map<String, DocumentBuilder<? extends BaseDocument>> documentBuilderRegistry = new ConcurrentHashMap<>();
+	Map<String, Collection<DocumentProvider<? extends BaseDocument>>> documentBuilderRegistry = new ConcurrentHashMap<>();
 
 	public TestRegistry() {
 	}
@@ -35,12 +36,15 @@ public class TestRegistry extends ClasspathAndBeanScanner {
 
 	@Override
 	public void handleBeanEntry(Object bean, String documentType) {
-		if (bean instanceof DocumentBuilder) {
-			documentBuilderRegistry.put(documentType, (DocumentBuilder<? extends BaseDocument>) bean);
+		if (bean instanceof DocumentProvider) {
+			Collection<DocumentProvider<? extends BaseDocument>> documentProviders = documentBuilderRegistry.get(documentType);
+			if (documentProviders == null) documentProviders = new ArrayList<>();
+			documentProviders.add((DocumentProvider<? extends BaseDocument>) bean);
+			documentBuilderRegistry.put(documentType, documentProviders);
 		}
 	}
 
-	public DocumentBuilder<? extends BaseDocument> getDocumentBuilder(String documentType) {
+	public Collection<DocumentProvider<? extends BaseDocument>> getDocumentBuilders(String documentType) {
 		return documentBuilderRegistry.get(documentType);
 	}
 
