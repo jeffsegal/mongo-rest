b6d6af95320e2820241b51e1ee081670d1896573
Jeff Segal <jeffrey.segal@gmail.com>
2014-05-13 10:09:47 -0400

    Changes to handleClasspathEntry signature

--- mongo-rest-persistence/src/main/java/com/segal/mongorest/core/util/ApplicationRegistry.java
+++ mongo-rest-persistence/src/main/java/com/segal/mongorest/core/util/ApplicationRegistry.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,6 +32,10 @@ public class ApplicationRegistry extends ClasspathAndBeanScanner {
 			ConcurrentHashMap<>();
 
 	public ApplicationRegistry() {
+	}
+
+	public ApplicationRegistry(String... packages) {
+		super(Arrays.asList(packages));
 	}
 
 	public ApplicationRegistry(List<String> packages) {
